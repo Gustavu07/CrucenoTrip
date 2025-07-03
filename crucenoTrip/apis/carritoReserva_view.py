b86c9436514c9535edf83139cadc53c9387d445e
Gustavo <110668274+Gustavu07@users.noreply.github.com>
2025-07-03 01:36:52 -0400

    subiendo funcional sin stripe

--- crucenoTrip/apis/carritoReserva_view.py
+++ crucenoTrip/apis/carritoReserva_view.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers, viewsets, status
+from rest_framework import serializers, viewsets
 from rest_framework.permissions import IsAuthenticated
 from crucenoTrip.models import CarritoReserva
 
